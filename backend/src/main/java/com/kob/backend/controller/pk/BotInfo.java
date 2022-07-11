1d9edb6374c8e77ca57793fe5c04bcf624ea8c00
Chi Liu <893606114@qq.com>
2022-07-11 21:09:45 +0800

    2022/7/11

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfo.java
@@ -0,0 +1,32 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfo {
+    @RequestMapping("getbotinfo/")
+    public String getBotInfo(){
+        return "Hhhh";
+    }
+    @RequestMapping("list/")
+    public List<String> getString(){
+        List<String> list = new LinkedList<>();
+        list.add("asdf");
+        list.add("xxxx");
+        return list;
+    }
+    @RequestMapping("map/")
+    public Map<String,String> getStrings(){
+        Map<String,String> map = new HashMap<>();
+        map.put("name","tigger");
+        map.put("name1","rubbit");
+        return map;
+    }
+}
